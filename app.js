59b61d7967c86b813251d415162245f2ab88e663
Martin Falatic <martin.falatic@gmail.com>
2021-12-20 02:01:31 -0800

    Add the PDF example from Google; other cleanups

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ async function main() {
     console.log('app listening on port ' + port);
     });
 
-    // Start puppeteer and screenshot a webpage
+    // Start puppeteer and perform some operations
     // Based on https://developers.google.com/web/tools/puppeteer/get-started
 
     // /myapp/node_modules/puppeteer/.local-chromium/linux-938248/chrome-linux/chrome
@@ -49,14 +49,28 @@ async function main() {
         });
         console.log('browser launched');
 
-        const page = await browser.newPage();
-        await page.goto("https://www.example.com");
-        const ss = await page.screenshot({path: "/opt/app/output/screenshot.png"});
+        // Take a screenshot of a page
+        const page_ss = await browser.newPage();
+        await page_ss.goto('https://www.example.com', {
+            waitUntil: 'networkidle2',
+        });
+        const call_ss = await page_ss.screenshot({path: '/opt/app/output/screenshot.png'});
+        await page_ss.close();
 
-        await page.close();
+        // Save a page as PDF
+        const page_pdf = await browser.newPage();
+        await page_pdf.goto('https://news.ycombinator.com', {
+            waitUntil: 'networkidle2',
+        });
+        const call_pdf = await page_pdf.pdf({ path: '/opt/app/output/screenshot.pdf', format: 'a4' });
+        await page_pdf.close();
+
         await browser.close();
         console.log('browser closed');
     }
+
+    // The following is a bad idea when the container is set to restart!
+    // process.exit(0);
 }
 
 main();
